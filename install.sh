b07ba1473eb6423dbe45783a76bace0742fd3877
Fabian Beltran <fabianbeltran1224@mail.com>
2024-04-22 22:48:59 -0500

    .zshrc && install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,5 @@
 ln -s ~/dotfiles/.gitconfig .
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
+echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
+mkdir -p ~/.fonts 
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/Meslo.zip -i /.fonts
